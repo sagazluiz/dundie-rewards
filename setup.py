d69ddb08e783e4463085356dbbcf866be5b8d3e9
sagazluiz <sagazluiz@hotmail.com>
2023-06-28 23:13:25 -0300

    Correction yaml

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Bruno Rocha, Fabio Araujo",
+    python_requires=">=3.8",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
