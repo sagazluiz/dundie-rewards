92d28257a56c54d3a4220f802d08ae68985ac508
sagazluiz <sagazluiz@hotmail.com>
2025-05-01 16:46:13 -0300

    Apply black formatting

--- dundie/utils/log.py
+++ dundie/utils/log.py
@@ -5,8 +5,7 @@ from logging import handlers
 LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.getLogger("dundie")
 fmt = logging.Formatter(
-    "%(asctime)s  %(name)s  %(levelname)s "
-    "l:%(lineno)d f:%(filename)s: %(message)s"
+    "%(asctime)s  %(name)s  %(levelname)s " "l:%(lineno)d f:%(filename)s: %(message)s"
 )
 
 
